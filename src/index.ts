1ff4b25c1c6a4d6c9a16d9fe2692eb4b5b9f9602
yeopseung <kwak991021@gmail.com>
2024-08-13 11:10:33 +0900

    📦초기 세팅 수정

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
