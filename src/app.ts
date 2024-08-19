e8f16acfd821cc5d4359b78484e65199c70dd75e
yeopseung <kwak991021@gmail.com>
2024-08-19 21:22:48 +0900

    🔨 app.ts & maridb.ts 코드 삭제

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import { AppDataSource } from "./config/datasource";
-import { User } from "./entity/User";
 
 const app = express();
 app.use(express.json());
 
 const authRouter = require("./routes/authRoute");
 const checkRouter = require("./routes/checkRoute");
+const boardRouter = require("./routes/boardRoute");
 
 app.use("/", authRouter);
 app.use("/check", checkRouter);
+app.use("/boards", boardRouter);
 
 // 데이터베이스 연결 초기화
 AppDataSource.initialize()
@@ -17,21 +18,5 @@ AppDataSource.initialize()
     console.log("Connected to the database");
   })
   .catch(error => console.error("Database connection error:", error));
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-//* DB 테스트용 (삭제 예정)
-app.get("/test-users", async (req, res) => {
-  try {
-    const userRepository = AppDataSource.getRepository(User);
-    const users = await userRepository.find(); // 모든 유저 데이터 조회
-    res.json(users);
-  } catch (error) {
-    console.error("Error querying users:", error);
-    res.status(500).send("Server Error");
-  }
-});
 
 export default app;
